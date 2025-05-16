cf72a50c8e5c3387c6eb12fb7ff745b033bc0408
Eduardo <cojr.eduardo@gmail.com>
2025-05-16 13:40:07 +0200

    changed it back to Pool

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,19 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Database connection failed:", err);
     return;
   }
   console.log("Successfully connected to MySQL");
+  connection.release();
 });
 
-export default connection;
+export default pool;
